dc95fb81853ef4f2f38bb5c5dc608bdf4fdb4868
randomfact236 <randomfact236@gmail.com>
2026-01-04 16:40:13 +0545

    Remove scripts shims

--- scripts/plan_status.cjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-// shim: forward to plan/plan_status.cjs
-const child_process = require('child_process');
-const path = require('path');
-const target = path.join(__dirname, '..', 'plan', 'plan_status.cjs');
-const args = process.argv.slice(2);
-const cp = child_process.spawn(process.execPath, [target, ...args], { stdio: 'inherit' });
-cp.on('exit', code => process.exit(code));
